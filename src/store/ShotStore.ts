6b1bd43e53f4f40f2ba049eb61d588e950d14690
Ian Worley <ian@ianworley.xyz>
2024-01-02 22:52:17 -0600

    Add ShotStore module

---
+++ src/store/ShotStore.ts
@@ -0,0 +1,35 @@
+import type { shot } from "@prisma/client";
+import { create } from "zustand";
+
+interface ShotStore {
+  shots: shot[];
+  addShots: (shots: shot) => void;
+  removeShot: (shot_id: number) => void;
+  updateShot: (shot: shot, user_id: number) => void;
+  addMultipleShots: (shots: shot[]) => void;
+}
+
+export const useShotStore = create<ShotStore>((set) => ({
+  shots: [],
+  addShots: (shots: shot) =>
+    set((state) => ({
+      shots: [...state.shots, shots],
+    })),
+
+  // add multiple shots as an array
+  addMultipleShots: (shots: shot[]) =>
+    set((state) => ({
+      shots: [...state.shots, ...shots],
+    })),
+
+  removeShot: (shot_id: number) =>
+    set((state) => ({
+      shots: state.shots.filter((shot) => shot.id !== shot_id),
+    })),
+
+  updateShot: (shot: shot, user_id: number) => {
+    set((state) => ({
+      shots: state.shots.map((s) => (s.id === shot.id ? shot : s)),
+    }));
+  },
+}));
