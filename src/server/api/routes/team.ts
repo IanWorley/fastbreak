74ca327ad7b817eb68023de5f709141400db58bd
Ian Worley <ian@ianworley.xyz>
2024-01-07 10:48:33 -0600

    Add createTeam endpoint to teamsRouter

--- src/server/api/routes/team.ts
+++ src/server/api/routes/team.ts
@@ -150,4 +150,43 @@ export const teamsRouter = router({
         },
       });
     }),
+
+  createTeam: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(10, "10 s"),
+        analytics: false,
+
+        prefix: "@upstash/ratelimit",
+      });
+
+      const { success } = await ratelimit.limit(ctx.user.id);
+
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+          message: "Too many requests",
+        });
+      }
+
+      const teamName = z.coerce.string().min(3).max(255).safeParse(input);
+
+      if (!teamName.success) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid team name",
+        });
+      }
+
+      const team = await ctx.db.team.create({
+        data: {
+          name: teamName.data,
+          users_id: ctx.user.id,
+        },
+      });
+
+      return team;
+    }),
 });
