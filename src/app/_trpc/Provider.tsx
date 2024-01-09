5152c04942ed317dc0328d9147f1fb5d5aed8637
Ian Worley <ian@ianworley.xyz>
2024-01-08 18:22:29 -0600

    No middlware (#28)
    * Update package.json and PrismaClient.ts, remove migration_lock.toml and modify page routes
    
    * Update database provider to MySQL in schema.prisma
    
    * Refactor dashboard page and basketball court component, add team creation validation, and update navbar links
    
    * Add ratelimiting to API routes
    
    * Commented out ReactQueryDevtools in Provider component
    
    * Refactor Team component and add deleteTeam mutation
    
    * Add createTeam endpoint to teamsRouter
    
    * Refactor form in NewTeamFormClient component
    
    * Refactor BasketballCourt component and Model component
    
    Add points validation in addShots endpoint
    
    * Refactor imports in NewTeamFormClient.tsx
    
    * Refactor data handling in PlayerClient and Game page
    
    * Add Prisma generation to build script
    
    * Update trpc provider URL
    
    * Refactor URL handling in Provider and client.ts

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -3,21 +3,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 
-import { trpc } from "./client";
+import { getUrl, trpc } from "./client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const url =
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}` + "/api/trpc"
-      : "http://localhost:3000/api/trpc";
 
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: url,
+          url: getUrl(),
         }),
       ],
     })
--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -3,3 +3,13 @@ import { createTRPCReact } from "@trpc/react-query";
 import { type AppRouter } from "@/src/server/index";
 
 export const trpc = createTRPCReact<AppRouter>({});
+
+function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export function getUrl() {
+  return getBaseUrl() + "/api/trpc";
+}
