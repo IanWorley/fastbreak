0dcbaf96c9edda84d4ad3b81aaa3dc51c906e999
Ian Worley <ian@ianworley.xyz>
2024-01-10 21:31:12 -0600

    Add @vercel/analytics and @vercel/speed-insights packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "./_components/ThemeProvider";
 import { type Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "FastBreak Analytics ",
   description: "A revolutionary sports analytics platform for basketball.",
@@ -30,6 +31,8 @@ export default function RootLayout({
           >
             <TRPCReactProvider cookies={cookies().toString()}>
               {children}
+              <Analytics />
+              <SpeedInsights />
             </TRPCReactProvider>
           </ThemeProvider>
         </body>
