209efec63fc79363e95f5f35e57e3e420671f2de
Ian Worley <ian@ianworley.xyz>
2024-01-29 15:57:14 -0600

    Refactor game page component and extract Games component

--- src/app/(page-routes)/team/[id]/game/page.tsx
+++ src/app/(page-routes)/team/[id]/game/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "~/app/_components/shadcn/ui/button";
 import { db } from "~/server/db";
 import Link from "next/link";
 import { api } from "~/trpc/server";
+import type { game, shot } from "@prisma/client";
 interface Props {
   params: { id: number };
 }
@@ -28,6 +29,12 @@ async function deleteGame(formData: FormData) {
     },
   });
 
+  await db.shot.deleteMany({
+    where: {
+      gameId: id,
+    },
+  });
+
   await db.game.delete({
     where: {
       id: id,
@@ -43,61 +50,73 @@ async function Page(props: Props) {
   const data = await api.game.grabGames.query(z.coerce.number().parse(id));
 
   return (
-    <main className="">
+    <main className="overflow-y-scroll">
       <Navbar className="fixed" teamId={id} viewingTeam={true} />
-      <h1 className="block p-7 text-center text-5xl font-extrabold"> Games </h1>
-      <div className=" ">
-        <div className="grid-cols-2 md:grid">
-          {data.length >= 0 &&
-            data.map((game) => (
-              <div
-                className="mx-8   my-4  bg-primary-foreground  md:mx-32"
-                key={game.id}
-              >
-                <h3 className="p-4 text-center text-4xl font-semibold">
-                  {" "}
-                  {game.name}
-                </h3>
-                <div className="flex flex-row justify-center p-2 text-center">
-                  <h5 className="text-md font-semibold">
-                    Total Points:
-                    {game.shots.reduce(
-                      (total, currentVal) => total + currentVal.points,
-                      0,
-                    )}
-                  </h5>
-                </div>
-
-                <div className="flex flex-row  gap-3">
-                  <Link
-                    className="block h-full w-full"
-                    href={`/team/${id}/game/${game.id}`}
-                  >
-                    <Button className="h-full w-full">View</Button>
-                  </Link>
-                  <form className="h-full w-full" action={deleteGame}>
-                    <input type="hidden" name="id" value={game.id} />
-                    <Button
-                      variant={"destructive"}
-                      type="submit"
-                      className="h-full w-full"
-                    >
-                      Delete
-                    </Button>
-                  </form>
-                </div>
-              </div>
-            ))}
-        </div>
-        <div className="fixed bottom-0 right-0 z-10 m-4 p-4">
+      <div>
+        <div className="flex flex-col  items-center   justify-evenly p-10 sm:flex-row ">
+          <h1 className="p-4 text-3xl font-bold "> Games </h1>
           <Link href={`/team/${id}/game/new `}>
-            <Button className="" variant={"secondary"}>
-              Add Game
-            </Button>
+            <Button className="">Add Game</Button>
           </Link>
         </div>
+        <div className="flex grid-cols-2 flex-col md:grid">
+          {data.length >= 0 &&
+            data.map((game) => (
+              <Games key={game.id} game={game} shots={game.shots} id={id} />
+            ))}
+        </div>
+
+        {data.length === 0 && (
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-center text-2xl font-bold">
+              {" "}
+              No Games are Available{" "}
+            </h3>
+          </div>
+        )}
       </div>
     </main>
+  );
+}
+
+interface GameProps {
+  game: game;
+  shots: shot[];
+  id: number;
+}
+
+export async function Games(props: GameProps) {
+  const { game, shots, id } = props;
+
+  return (
+    <div className="mx-8   my-4  bg-primary-foreground  md:mx-32" key={game.id}>
+      <h3 className="p-4 text-center text-4xl font-semibold">{game.name}</h3>
+      <div className="flex flex-row justify-center p-2 text-center">
+        <h5 className="text-md font-semibold">
+          Total Points:
+          {shots.reduce((total, currentVal) => total + currentVal.points, 0)}
+        </h5>
+      </div>
+
+      <div className="flex flex-row  gap-3">
+        <Link
+          className="block h-full w-full"
+          href={`/team/${id}/game/${game.id}`}
+        >
+          <Button className="h-full w-full">View</Button>
+        </Link>
+        <form className="h-full w-full" action={deleteGame}>
+          <input type="hidden" name="id" value={game.id} />
+          <Button
+            variant={"destructive"}
+            type="submit"
+            className="h-full w-full"
+          >
+            Delete
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
 
