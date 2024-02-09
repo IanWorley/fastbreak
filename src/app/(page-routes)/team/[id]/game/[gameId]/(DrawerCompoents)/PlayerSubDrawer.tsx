a447937a0a0c47bc7d77346f86531ce62f4b6bb0
Ian Worley <ian@ianworley.xyz>
2024-02-09 11:34:00 -0600

    Inside PlayerSubDrawer Add X button to close Drawer (#86)

--- src/app/(page-routes)/team/[id]/game/[gameId]/(DrawerCompoents)/PlayerSubDrawer.tsx
+++ src/app/(page-routes)/team/[id]/game/[gameId]/(DrawerCompoents)/PlayerSubDrawer.tsx
@@ -1,6 +1,10 @@
+import { FaXmark } from "react-icons/fa6";
+import { Button } from "~/app/_components/shadcn/ui/button";
 import {
   Drawer,
+  DrawerClose,
   DrawerContent,
+  DrawerHeader,
   DrawerTitle,
 } from "~/app/_components/shadcn/ui/drawer";
 import { usePlayerForApp } from "~/store/PlayerForApp";
@@ -18,10 +22,16 @@ function PlayerSubDrawer(props: IPropsPlayerSubDrawer) {
   return (
     <Drawer open={open} onOpenChange={(o) => onOpenChange(o)}>
       <DrawerContent>
-        <DrawerTitle>
-          {" "}
-          <h3 className="p-4 text-2xl font-semibold">Player Sub In</h3>
-        </DrawerTitle>
+        <DrawerHeader className="flex items-center justify-between">
+          <DrawerTitle>
+            <h3 className="p-4 text-2xl font-semibold">Player Sub In</h3>
+          </DrawerTitle>
+          <DrawerClose className="hidden p-4 md:block">
+            <Button variant={"destructive"}>
+              <FaXmark />
+            </Button>
+          </DrawerClose>
+        </DrawerHeader>
         <div className="grid grid-cols-4 grid-rows-4 gap-5">
           {players
             .filter((player) => player.isPlaying === false)
