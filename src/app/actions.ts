0428d78e2de12f7d36b7c42f2716d973295bbb9e
Turbobot <turbobot@vercel.com>
2024-02-13 12:11:44 -0600

    feat(create-turbo): apply pnpm-eslint transform

--- src/app/actions.ts
+++
@@ -1,9 +0,0 @@
-import { revalidatePath } from "next/cache";
-
-export async function refreshGamePage(id: string) {
-  revalidatePath(`/team/${id}/game`);
-}
-
-export async function refreshTeamPage() {
-  revalidatePath(`/team/`);
-}
