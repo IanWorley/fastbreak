71bdb3626ad90585d1659e9c0a721423cf18257f
Ian Worley <ian@ianworley.xyz>
2024-02-22 23:18:17 -0600

    Add new game page components

---
+++ apps/nextjs/src/app/(page-routes)/team/[id]/game/[gameId]/(DrawerCompoents)/PlayerSubDrawer.tsx
@@ -0,0 +1,50 @@
+import { FaXmark } from "react-icons/fa6";
+import { Button } from "~/app/_components/shadcn/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from "~/app/_components/shadcn/ui/drawer";
+import { usePlayerForApp } from "~/store/PlayerForApp";
+import DrawerPlayerCard from "./DrawerPlayerCard";
+interface IPropsPlayerSubDrawer {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  playerToSwap: string;
+}
+
+function PlayerSubDrawer(props: IPropsPlayerSubDrawer) {
+  const { open, onOpenChange, playerToSwap } = props;
+  const { players } = usePlayerForApp((state) => state);
+
+  return (
+    <Drawer open={open} onOpenChange={(o) => onOpenChange(o)}>
+      <DrawerContent>
+        <DrawerHeader className="flex items-center justify-between">
+          <DrawerTitle className="text-2xl">Player Sub In</DrawerTitle>
+          <DrawerClose className="hidden p-4 md:block">
+            <Button variant={"destructive"}>
+              <FaXmark />
+            </Button>
+          </DrawerClose>
+        </DrawerHeader>
+        <div className="flex grid-cols-4 grid-rows-4  flex-col gap-5 overflow-y-scroll md:grid">
+          {players
+            .filter((player) => player.isPlaying === false)
+            .map((player) => (
+              <DrawerPlayerCard
+                key={player.id}
+                player={player}
+                playerToSwap={playerToSwap}
+                onOpenChange={onOpenChange}
+              />
+            ))}
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default PlayerSubDrawer;
---
+++ apps/nextjs/src/app/(page-routes)/team/[id]/game/[gameId]/error.tsx
@@ -0,0 +1,36 @@
+"use client"; // Error components must be Client Components
+
+import { notFound } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  // trpc grab status code from error and pass it to notFound
+  if (error?.digest === "NEXT_NOT_FOUND") {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
