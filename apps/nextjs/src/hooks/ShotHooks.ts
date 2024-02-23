43a32c71d6a0cf663943a264f8a293f550183dc5
Ian Worley <ian@ianworley.xyz>
2024-02-22 23:19:22 -0600

    Add useShotsForGame hook

---
+++ apps/nextjs/src/hooks/ShotHooks.ts
@@ -0,0 +1,13 @@
+import { api } from "~/trpc/react";
+
+export const useShotsForGame = (gameId: string, teamId: string) => {
+  const shots = api.game.grabPlayersShotsFromGame.useQuery(
+    { gameId, teamId },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return shots.data ?? [];
+};
