f311573a50f600fa9953b16c5db6a84ed9ca45af
Ian Worley <ian@ianworley.xyz>
2024-02-27 14:37:05 -0600

    Add dependencies and remove unused code

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,12 +1,8 @@
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
-import { api } from "~/utils/api";
-
 export default function Index() {
-  const utils = api.useUtils();
-
   return (
     <SafeAreaView className=" bg-background">
       {/* Changes page title visible on the header */}
@@ -16,19 +12,11 @@ export default function Index() {
           Create <Text className="text-primary">T3</Text> Turbo
         </Text>
 
-        <Pressable
-          onPress={() => void utils.post.all.invalidate()}
-          className="flex items-center rounded-lg bg-primary p-2"
-        >
-          <Text className="text-foreground"> Refresh posts</Text>
-        </Pressable>
-
         <View className="py-2">
           <Text className="font-semibold italic text-primary">
             Press on a post
           </Text>
         </View>
-        {/* <CreatePost /> */}
       </View>
     </SafeAreaView>
   );
--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,12 +1,9 @@
 import { gameRouter } from "./router/game";
 import { playerRouter } from "./router/player";
-import { postRouter } from "./router/post";
 import { teamsRouter } from "./router/team";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  // auth: authRouter,
-  post: postRouter,
   team: teamsRouter,
   game: gameRouter,
   player: playerRouter,
