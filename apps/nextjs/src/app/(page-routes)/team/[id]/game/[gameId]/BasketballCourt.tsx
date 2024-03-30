08ad0a42a4fe0a2940b08b9be8a60d43fecaed9a
Ian Worley <ian@ianworley.xyz>
2024-03-30 08:10:25 -0500

    Fix basketball court rendering and remove unnecessary code

--- apps/nextjs/src/app/(page-routes)/team/[id]/game/[gameId]/BasketballCourt.tsx
+++ apps/nextjs/src/app/(page-routes)/team/[id]/game/[gameId]/BasketballCourt.tsx
@@ -50,8 +50,9 @@ const BasketballCourt: React.FC<BasketballCourtProps> = (
   useEffect(() => {
     if (!fetchShots) return;
     const activeShots = fetchShots.filter((shots) => {
+      // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
       const player = players.find(
-        (player) => player.id === shots.player_Id && shots.game_Id === gameId,
+        (player) => player.id === shots?.player_Id && shots?.game_Id === gameId,
       );
       return player && player.isPlaying;
     });
