69fd12de918a93e397dbe8a3782c6ae706dffc42
Ian Worley <ian@ianworley.xyz>
2024-05-22 22:19:21 -0500

    chore: Update drizzle.config.ts to use SQLite dialect

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,10 +3,12 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema",
   driver: "turso",
+
   dbCredentials: {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   },
   tablesFilter: ["t3turbo_*"],
+  dialect: "sqlite",
 } satisfies Config;
